400e812245f475c4aa7b420854bc46d4df2cbf45
Arman2409 <arman.ghazaryan@hylink.am>
2024-05-01 00:49:29 +0400

    feat: mocking and testing element added

--- jest.config.js
+++ jest.config.js
@@ -3,10 +3,6 @@ export default {
     transform: {
         '^.+\\.(ts|tsx)?$': 'ts-jest',
         '^.+\\.(js|jsx)$': 'babel-jest',
-        "^.+\\.svg$": "jest-svg-transformer",
-        ".+\\.(svg|css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$": "jest-transform-stub",
     },
-    moduleNameMapper: {
-        "^.+\\.svg$": "/fileMock.ts",
-    }
+    testEnvironment: "jsdom",
 };
--- test/client/getColors.spec.ts
+++ test/client/getColors.spec.ts
@@ -1,29 +1,13 @@
-// Import your getSvgColors function
-import getSvgColors from '../../src/client/getColors'; // Assuming getSvgColors is in the same directory
-import svgForTest from "/assets/test/eye.svg";
-// import { JSDOM } from "jsdom";
+import getColors from "../../src/client/getColors";
+import mockElement from "../_utils/mockElement";
 
-const fs: any = jest.genMockFromModule("fs");
+const mockedSVG = mockElement("svg");
+const mockedPath = mockElement("path", { fill: "red" })
 
-jest.mock("fs");
+jest.spyOn(mockedSVG, 'querySelectorAll').mockReturnValue([mockedPath] as any);
 
-test('getSvgColors should extract colors from SVG', async () => {
-    const svgContent = await fs.readFileSync('../../assets/test/eye.svg', 'utf-8')
-    console.log(svgContent);
-    console.log(svgForTest);
+test('getColors should extract colors from SVG', () => {
+    const colors = getColors(mockedSVG);
 
-
-
-    // const dom = new JSDOM(svgContent);
-    // const window = dom.window;
-    // global.document = window.document;
-
-    // Call the function with the SVG content
-    // const colors = getSvgColors(svgContent);
-
-    // Assertions
-    // expect(colors).toContain('#FF0000'); // Red from rect fill
-    // expect(colors).toContain('#00FF00'); // Green from circle stroke
-    // expect(colors).toContain('#0000FF'); // Blue from text style
-    expect(true).toBeTruthy();
+    expect(colors).toEqual(['red']);
 });
