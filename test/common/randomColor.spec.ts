7c65c602e879895bbeb79111ba2f6a74b72326df
Arman2409 <arman.ghazaryan@hylink.am>
2024-05-01 16:53:42 +0400

    feat: add randomColor function

---
+++ test/common/randomColor.spec.ts
@@ -0,0 +1,17 @@
+import randomColor from "../../src/common/randomColor";
+
+describe('randomColor', () => {
+    test("should return HEX value for HEX option", () => {
+        const hexColor = randomColor("hex");
+
+        expect(hexColor.startsWith("#")).toBeTruthy();
+        
+    })
+
+    test("should return RGB value for RGB option", () => {
+        const rgbColor = randomColor("rgb");
+
+        expect(rgbColor.startsWith("rgb")).toBeTruthy();
+        
+    })
+});
